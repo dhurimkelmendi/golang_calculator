69883548e9e515e77fcf0679c04cd830f035488b
Dhurim Kelmendi <dhkelmendi@gmail.com>
2017-10-14 11:43:18 +0200

    Define a method for converting string input to int and to float64, remove io package from app structure

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
-import "fmt"
-
+import (
+        "calculator/calc"
+	"fmt"
+	"strconv"
+	"os"
+)
 func main() {
-    //define a calc package for the calculator methods
-    //define a package for I/O
+
+        var operation string
+	fmt.Println("Please select an operation: +, -, *, /")
+        fmt.Scanln(&operation)
+
+	var num1 string
+	fmt.Println("Please input the first number")
+	fmt.Scanln(&num1)
+
+	var num2 string
+	fmt.Println("Please input the second number")
+	fmt.Scanln(&num2)
+
+	firstNumber := stringToInt(num1)
+	secondNumber := stringToInt(num2)
+	firstFloat := stringToFloat64(num1)
+	secondFloat := stringToFloat64(num2)
+	fmt.Println(calc.Add(firstNumber, secondNumber))
+	fmt.Println(calc.Subtract(firstNumber, secondNumber))
+	fmt.Println(calc.Multiply(firstFloat, secondFloat))
+	fmt.Println(calc.Divide(firstFloat, secondFloat))
+
 }
 
+func stringToInt(str string) int {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	return i
+}
+
+
+func stringToFloat64(str string) float64 {
+	i, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	return i
+}
