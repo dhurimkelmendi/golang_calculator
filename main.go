6cad0cc6c0d9ecee14f35c705c8529cd4fe48f37
Dhurim Kelmendi <dhkelmendi@gmail.com>
2017-10-14 12:11:30 +0200

    Define the workflow via switch for the operation selected, fix output styles

--- main.go
+++ main.go
@@ -7,28 +7,47 @@ import (
 	"os"
 )
 func main() {
-
+	getOperation:
         var operation string
-	fmt.Println("Please select an operation: +, -, *, /")
+	fmt.Print("Please select an operation: +, -, *, / : ")
         fmt.Scanln(&operation)
 
 	var num1 string
-	fmt.Println("Please input the first number")
+	fmt.Print("Please input the first number: ")
 	fmt.Scanln(&num1)
 
 	var num2 string
-	fmt.Println("Please input the second number")
+	fmt.Print("Please input the second number: ")
 	fmt.Scanln(&num2)
 
-	firstNumber := stringToInt(num1)
-	secondNumber := stringToInt(num2)
-	firstFloat := stringToFloat64(num1)
-	secondFloat := stringToFloat64(num2)
-	fmt.Println(calc.Add(firstNumber, secondNumber))
-	fmt.Println(calc.Subtract(firstNumber, secondNumber))
-	fmt.Println(calc.Multiply(firstFloat, secondFloat))
-	fmt.Println(calc.Divide(firstFloat, secondFloat))
+	switch operation {
+	case "+":
+		var firstNumber int = stringToInt(num1)
+		var secondNumber int = stringToInt(num2)
+		fmt.Print("Result: ")
+		fmt.Println(calc.Add(firstNumber, secondNumber))
+	case "-":
+		var firstNumber int = stringToInt(num1)
+		var secondNumber int = stringToInt(num2)
+		fmt.Print("Result: ")
+		fmt.Println(calc.Subtract(firstNumber, secondNumber))
 
+	case "*":
+		var firstNumber float64 = stringToFloat64(num1)
+		var secondNumber float64 = stringToFloat64(num2)
+		fmt.Print("Result: ")
+		fmt.Println(calc.Multiply(firstNumber, secondNumber))
+
+	case "/":
+		var firstNumber float64 = stringToFloat64(num1)
+		var secondNumber float64 = stringToFloat64(num2)
+		fmt.Print("Result: ")
+		fmt.Println(calc.Divide(firstNumber, secondNumber))
+
+	default:
+		fmt.Println("Invalid operation selected. Please try again!")
+		goto getOperation
+	}
 }
 
 func stringToInt(str string) int {
@@ -40,12 +59,11 @@ func stringToInt(str string) int {
 	return i
 }
 
-
 func stringToFloat64(str string) float64 {
-	i, err := strconv.ParseFloat(str, 64)
+	f, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
-	return i
+	return f
 }
